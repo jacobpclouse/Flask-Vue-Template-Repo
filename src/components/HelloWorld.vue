5be3cbc2a96349bee78609f59adea05ee4348e5b
Jacob Clouse <58919856+jacobpclouse@users.noreply.github.com>
2023-04-10 10:38:26 -0400

    added files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form @submit.prevent="sendMessage">
+      <input type="text" v-model="message">
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      message: ''
+    };
+  },
+  methods: {
+    sendMessage() {
+      axios.post('http://127.0.0.1:5000/message', { message: this.message })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
